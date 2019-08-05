e822bb032ef6811e54ec18c0da9bd190710e315c
thuydungnguyen.ictk63@gmail.com <thuydungnguyen.ictk63@gmail.com>
2019-08-05 22:11:13 +0700

    Demo

--- Blog/settings.py
+++ Blog/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/dung/workspace/python/greenblogs/blogs/static'
 LOGOUT_REDIRECT_URL = '/'
